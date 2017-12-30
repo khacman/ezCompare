c5e0fb42de3b74bd817bdc32bcc7f56af0113f68
Khac Man <khacmanle@gmail.com>
2017-12-30 10:15:43 +0700

    Update Lambda API URL

--- src/config.js
+++ src/config.js
@@ -1,9 +1,8 @@
-"use strict";
 const servers = {
     local: "http://localhost:5000/getPages",
-    aws: "https://pmcivzfj3j.execute-api.ap-southeast-1.amazonaws.com/prod/getPages"
+    aws: "https://hrag2hvbg7.execute-api.ap-southeast-1.amazonaws.com/prod/getPages"
 };
 
 export const config = {
-    apiUrl: servers.local
+    apiUrl: servers.aws
 };
