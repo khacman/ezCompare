cfca4e2bb12caa7d2c2ff9a7b7beb4468f1e6f61
Khac Man <khacmanle@gmail.com>
2017-12-30 13:44:32 +0700

    [UI] Implement client side caching. Reduce requests to server & save money for Lambda function. Yay!

--- src/services/productService.js
+++ src/services/productService.js
@@ -2,31 +2,54 @@ import messageFactory from "./messageFactory";
 import {PhoneDomAdapter} from "../models/Phone";
 import {config} from "../config";
 
+const domCache = new Map();
+
 function productService() {
 
     function getProducts(urls) {
-
-        const payload = messageFactory.createGetPageMessage(urls);
-        let productList = [];
-
-        const reqOptions = {
-            method: "POST",
-            headers: new Headers({"Content-Type": "application/json"}),
-            body: payload.toString()
-        };
-
-        return fetch(config.apiUrl, reqOptions).then((response) => {
-            return response.json();
-        }).then((data) => {
-
-            const parser = new DOMParser();
-            // const doc = parser.parseFromString(data.documents[0], "text/html");
-            const docs = data.documents.map(doc => parser.parseFromString(doc, "text/html"));
-            docs.forEach(doc => {
-                productList.push(new PhoneDomAdapter(doc));
-            });
-            return productList;
+        const productList = [];
+        let requestUrls = [];
+        let cachedUrls = [];
+        urls.forEach(url => {
+            if (domCache.has(url)) {
+                cachedUrls.push(url);
+            } else {
+                requestUrls.push(url);
+            }
         });
+
+        if (requestUrls.length === 0) { // All requests are cached. No request to server required
+            return new Promise(resolve => {
+                cachedUrls.forEach(url => {
+                    productList.push(domCache.get(url));
+                });
+                resolve(productList);
+            });
+        } else {
+            const payload = messageFactory.createGetPageMessage(requestUrls);
+            const reqOptions = {
+                method: "POST",
+                headers: new Headers({"Content-Type": "application/json"}),
+                body: payload.toString()
+            };
+            return fetch(config.apiUrl, reqOptions).then((response) => {
+                return response.json();
+            }).then((data) => {
+                const parser = new DOMParser();
+                const docs = data.documents.map(doc => parser.parseFromString(doc, "text/html"));
+                docs.forEach((doc, i) => {
+                    const phone = new PhoneDomAdapter(doc);
+                    domCache.set(requestUrls[i], phone); // cache result
+                    productList.push(phone);
+                });
+                // Add cached products to result
+                cachedUrls.forEach(url => {
+                    productList.push(domCache.get(url));
+                });
+
+                return productList;
+            });
+        }
     }
 
     return {
